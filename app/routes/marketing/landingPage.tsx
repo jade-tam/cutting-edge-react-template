418b0b90d95549a68223c32c4bf56473bbb617ad
jade-tam <jade.tam.original@gmail.com>
2026-03-03 16:00:17 +0700

    feat: configure iconify tailwind css with fluent icon set

--- app/routes/marketing/landingPage.tsx
+++ app/routes/marketing/landingPage.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-bold">
         {m.example_message({ username: "Jade" })}
+        <span className="icon-[fluent--alert-snooze-24-filled]"></span>
       </h1>
       <NavLink to="/auth/login">{m.go_to_login()}</NavLink>
     </div>
