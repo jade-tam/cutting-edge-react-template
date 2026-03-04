74c4f833bed37be1ec0aa243b3e90223a0c3f428
jade-tam <jade.tam.original@gmail.com>
2026-03-04 17:18:41 +0700

    feat: config SSR for some pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,10 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  // return a list of URLs to prerender at build time
+  async prerender() {
+    return ["/", "/about", "/contact"];
+  },
   // Follow https://inlang.com/m/gerre34r/library-inlang-paraglideJs/react-router#server-side-rendering-using-middleware
   future: {
     v8_middleware: true,
